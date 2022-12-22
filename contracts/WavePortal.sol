daebdb911bfbd462b5639ec7d3ccb4e9c8550f57
Aziz Abdullaev <aziz.saidmuratov@duke.edu>
2022-12-21 22:54:13 -0800

    add global total waves variable, two functions, and script to run them

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,18 @@ pragma solidity ^0.8.17;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaves; // state variable
     constructor() {
         console.log("Salom, World!");
+    }
+
+    function wave() public{
+        totalWaves += 1;
+        console.log("%s waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256){
+        console.log("We have %d waves in total", totalWaves);
+        return totalWaves;
     }
 }
