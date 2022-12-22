cf0791de56430a058a5d95a078e9be9c8495ceb0
Aziz Abdullaev <aziz.saidmuratov@duke.edu>
2022-12-21 23:31:21 -0800

    add mapping to count total waves by user, increase wave count per user in wave()

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -6,13 +6,20 @@ import "hardhat/console.sol";
 
 contract WavePortal {
     uint256 totalWaves; // state variable
+
+    mapping (address => uint104) public waveChart; // to store who waved most
+
     constructor() {
         console.log("Salom, World!");
     }
 
-    function wave() public{
+    function wave() public {
         totalWaves += 1;
+        uint104 waver = waveChart[msg.sender];
+        waver += 1;
+        waveChart[msg.sender] = waver;
         console.log("%s waved!", msg.sender);
+        console.log("%s waved in total %d times", msg.sender, waveChart[msg.sender]);
     }
 
     function getTotalWaves() public view returns (uint256){
