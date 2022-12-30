5c1e1d390d433771e269c8da47f1140850b2388e
Aziz Abdullaev <aziz.saidmuratov@duke.edu>
2022-12-29 22:38:57 -0800

    add connection to metamask wallet

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,80 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
+const getEthereumObject = () => window.ethereum;
+
+/* 
+  Find linked account. Found ? return first one : return null
+*/
+
+const findMetamaskAccount = async () => {
+  try {
+    const ethereum = getEthereumObject();
+
+    // check access to ethereum object
+    if (!ethereum) {
+      console.error("No metamask found!");
+    }
+
+    console.log("We have Ethereum object", ethereum);
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account", account);
+      return account;
+    } else {
+      console.error("No authorized accounts found");
+      return null;
+    }
+
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  const connectWallet = async () => {
+    try {
+      const ethereum = getEthereumObject();
+
+      if (!ethereum) {
+        alert("This site requries metamask. Please download and install it");
+        return;
+      }
+
+      const accounts = await ethereum.request({method: "eth_requestAccounts"});
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  /*
+   * The passed callback function will be run when the page loads.
+   * More technically, when the App component "mounts".
+   */
+  useEffect(() => {
+    const getAccount = async () => await findMetamaskAccount();
+    const account = getAccount();
+    if (account !== null) {
+      setCurrentAccount(account);
+    }
+  }, []);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 2)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        BADONGAGOND
-      </p>
+    <div className="container mx-auto">
+      <h1 className='text-slate-800 font-bold text-3xl mb-2'>Salom!</h1>
+
+      {!currentAccount && (
+        <button onClick={connectWallet} className="px-4 py-3 rounded-sm ring-1 ring-sky-500">Connect Wallet</button>
+      )}
     </div>
   )
 }
