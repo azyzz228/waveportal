eb6cbb1552354d314e2ec41916ff46ddb4bdba0f
Aziz Abdullaev <aziz.saidmuratov@duke.edu>
2023-01-03 01:38:09 -0800

    add connection status, total waves, status

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ const findMetamaskAccount = async () => {
 
     // check access to ethereum object
     if (!ethereum) {
-      console.error("No metamask found!");
+      alert("No metamask found!");
     }
 
     console.log("We have Ethereum object", ethereum);
@@ -38,10 +38,15 @@ const findMetamaskAccount = async () => {
 
 function App() {
 
-  const [currentAccount, setCurrentAccount] = useState("");
+  const [currentAccount, setCurrentAccount] = useState(null);
+  const [totalWaves, setTotalWaves] = useState(null);
+  const [status, setStatus] = useState('Not connected');
+
 
   const contractAddress = '0x1aeCb3450907604a70198e71EF140DA03343940C';
   const contractABI = abi.abi;
+
+
 
   const connectWallet = async () => {
     try {
@@ -62,6 +67,7 @@ function App() {
   }
 
   const wave = async () => {
+    setStatus('loading')
     try {
       // get ethereum object
       const { ethereum } = window;
@@ -70,15 +76,36 @@ function App() {
         const provider = new ethers.providers.Web3Provider(ethereum);
         const signer = provider.getSigner();
         const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+    
+        const waveTxn = await wavePortalContract.wave();
+        setStatus('Mining...')
+        console.log("Mining ...", waveTxn);
 
+        await waveTxn.wait();
+        console.log("Mined -- ", waveTxn);
+        setStatus('Mined')
         let count = await wavePortalContract.getTotalWaves();
-        console.log("Retrieved total wave count ...", count.toNumber());
+        console.log("NEW Retrieved total wave count ...", count.toNumber());
+        setTotalWaves(count.toNumber());
+
       } else {
         console.log("Ethereum object does not exist!");
       }
     } catch (error) {
       console.error(error);
+      setStatus('error');
     }
+  }
+
+  const getTotalWaves = async () => {
+    const { ethereum } = window;
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    const signer = provider.getSigner();
+    const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+  
+    let count = await wavePortalContract.getTotalWaves();
+    console.log("OLD retrieved total wave count ...", count.toNumber());
+    setTotalWaves(count.toNumber());
   }
 
   /*
@@ -86,22 +113,32 @@ function App() {
    * More technically, when the App component "mounts".
    */
   useEffect(() => {
+    setStatus('loading')
     const getAccount = async () => await findMetamaskAccount();
     const account = getAccount();
     if (account !== null) {
-      setCurrentAccount(account);
+      account.then(r => {
+        setCurrentAccount(r);
+      })
+      getTotalWaves();
     }
+    setStatus('Loaded')
   }, []);
 
   return (
-    <div className="container mx-auto p-16">
-      <h1 className='text-slate-800 font-bold text-3xl mb-2'>Salom!</h1>
+    <div className="container mx-auto p-16 font-mono">
+      <h1 className='text-slate-800 font-bold text-base mb-2'>Salom <span className='text-base font-mono font-normal text-sky-500'>{`${currentAccount ? currentAccount+'!' : '!'}`}</span></h1>
 
+<div className="flex flex-row space-x-4 mb-12">
+{currentAccount ? <p className='px-2 py-2 bg-green-100 text-green-800'>Connected</p> : <p className='px-2 py-2 bg-red-100 text-red-800'>Not Connected</p>}
+<p className={`p-2 transform ${currentAccount ? 'opacity-100' : 'opacity-5'}`}>Total Waves: <span className='text-sky-500'>{totalWaves ? totalWaves : ''}</span></p>
+<p className={`p-2 ${currentAccount ? 'opacity-100' : 'opacity-5'}`}>Status: {status}</p>
+</div>
       {!currentAccount && (
         <button onClick={connectWallet} className="px-4 py-3 rounded-sm ring-1 ring-sky-500">Connect Wallet</button>
       )}
 
-      <button className='px-4 py-3 rounded-sm ring-1 ring-sky-500 hover:bg-sky-300 ' onClick={() => wave()}>
+      <button className={`px-4 py-3 rounded-sm ring-1 ring-sky-500 hover:bg-sky-300`} onClick={() => wave()}>
         Wave at Me!
       </button>
     </div>
